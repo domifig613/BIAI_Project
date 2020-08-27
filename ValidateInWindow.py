ec5097d67013d28dec88822158c65c471c68550c
domifig613 <32535456+domifig613@users.noreply.github.com>
2020-08-27 18:54:36 +0200

    Move last version in main repository

---
+++ ValidateInWindow.py
@@ -0,0 +1,102 @@
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter import filedialog
+import tensorflow.keras as keras
+import os
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import tensorflow as tf
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+global img
+global filename
+global text
+global labels
+global model
+
+win = Tk()
+win.title("Pneumonia Checker")
+c = Canvas(win, width=400, height=400)
+c.pack()
+text = StringVar()
+path = os.getcwd()
+label = Label(win, textvariable=text)
+label.pack()
+height = 150
+width = 150
+path = r"C:\Users\domin\Desktop\BIAI\chest_data"
+train_dir = os.path.join(path, 'train')
+image_gen_train = ImageDataGenerator(
+    rescale=1. / 255,
+    rotation_range=30,
+    width_shift_range=.15,
+    height_shift_range=.15,
+    horizontal_flip=True,
+    zoom_range=0.5
+)
+
+train_data_gen = image_gen_train.flow_from_directory(batch_size=64,
+                                                     directory=train_dir,
+                                                     shuffle=True,
+                                                     target_size=(height, width),
+                                                     class_mode='binary')
+
+pathNames = train_data_gen.filenames
+
+labels = []
+for filename in pathNames:
+    newLabel = filename.split('\\')[0]
+    isNew = True
+    for label in labels:
+        if label == newLabel:
+            isNew = False
+            break
+    if isNew:
+        labels.append(newLabel)
+
+height = 150
+width = 150
+pf = r"C:\Users\domin\Desktop\BIAI\chest_data\train\NORMAL\IM-0115-0001.jpeg"
+
+model = tf.keras.models.load_model('model')
+
+batch_holder = np.zeros((1, height, width, 3))
+
+
+###########################################################################
+def loadPicture():
+    global filename
+    win.filename = filedialog.askopenfilename(initialdir="/", title="Select file",
+                                              filetypes=(("jpeg files", "*.jpeg"), ("all files", "*.*")))
+    global img
+    img = Image.open(win.filename)
+    img = ImageTk.PhotoImage(img.resize((150, 150), Image.ANTIALIAS))
+    c.create_image(20, 20, anchor=NW, image=img)
+
+    img = image.load_img(win.filename, target_size=(height, width))
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img = img.reshape(1, width, height, 3)
+    result = model.predict(img, batch_size=1)
+    index = 0
+    for i in result:
+        for j in i:
+            if j == 1:
+                break
+            index += 1
+    global text
+    text.set(labels[index])
+
+
+b = Button(win, text='Photo:', width=30, command=loadPicture)
+b.pack()
+
+win.mainloop()
---
+++ train.py
@@ -0,0 +1,196 @@
+import numpy as np
+import pandas as pd
+import cv2
+from keras.layers import Dense, Conv2D , MaxPool2D , Flatten , Dropout , BatchNormalization
+import matplotlib.pyplot as plt
+import seaborn as sns
+from keras.preprocessing.image import ImageDataGenerator
+import keras
+from keras.callbacks import ReduceLROnPlateau
+from keras.models import Sequential
+from sklearn.metrics import classification_report,confusion_matrix
+from sklearn.model_selection import train_test_split
+import os
+
+import os
+for path, _, files in os.walk('C:/Users/domin/Desktop/BIAI'):
+    for file in files:
+        os.path.join(path, file)
+
+tags = ['PNEUMONIA', 'NORMAL']
+image_size = 150
+
+def trainingData(data_dir):
+    data = []
+    for label in tags:
+        path = os.path.join(data_dir, label)
+        class_num = tags.index(label)
+        for img in os.listdir(path):
+            try:
+                img_arr = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+                resized_arr = cv2.resize(img_arr, (image_size, image_size)) # Reshaping images to preferred size
+                data.append([resized_arr, class_num])
+            except Exception as e:
+                print(e)
+    return np.array(data)
+
+test = trainingData('C:/Users/domin/Desktop/BIAI/chest_data/test')
+val = trainingData('C:/Users/domin/Desktop/BIAI/chest_data/val')
+train = trainingData('C:/Users/domin/Desktop/BIAI/chest_data/train')
+
+l = []
+for i in train:
+    if(i[1] != 0):
+        l.append("Normal")
+    else:
+        l.append("Pneumonia")
+sns.set_style('darkgrid')
+sns.countplot(l)
+
+plt.figure(figsize = (5,5))
+plt.imshow(train[0][0], cmap='gray')
+plt.title(tags[train[0][1]])
+
+plt.figure(figsize = (5,5))
+plt.imshow(train[-1][0], cmap='gray')
+plt.title(tags[train[-1][1]])
+
+x_test = []
+y_test = []
+
+x_train = []
+y_train = []
+
+x_val = []
+y_val = []
+
+for feature, label in train:
+    x_train.append(feature)
+    y_train.append(label)
+
+for feature, label in test:
+    x_test.append(feature)
+    y_test.append(label)
+
+for feature, label in val:
+    x_val.append(feature)
+    y_val.append(label)
+
+x_train = np.array(x_train) / 255
+x_val = np.array(x_val) / 255
+x_test = np.array(x_test) / 255
+
+x_train = x_train.reshape(-1, image_size, image_size, 1)
+y_train = np.array(y_train)
+
+x_val = x_val.reshape(-1, image_size, image_size, 1)
+y_val = np.array(y_val)
+
+x_test = x_test.reshape(-1, image_size, image_size, 1)
+y_test = np.array(y_test)
+
+dg = ImageDataGenerator(
+    featurewise_center=False,
+    samplewise_center=False,
+    featurewise_std_normalization=False,
+    samplewise_std_normalization=False,
+    zca_whitening=False,
+    rotation_range=20,
+    zoom_range=0.02,
+    width_shift_range=0.03,
+    height_shift_range=0.05,
+    horizontal_flip=True,
+    vertical_flip=False)
+
+dg.fit(x_train)
+
+mod = Sequential()
+mod.add(Conv2D(32, (3, 3), strides=1, padding='same', activation='relu', input_shape=(150, 150, 1)))
+mod.add(BatchNormalization())
+mod.add(MaxPool2D((2, 2), strides=2, padding='same'))
+mod.add(Conv2D(64, (3, 3), strides=1, padding='same', activation='relu'))
+mod.add(Dropout(0.1))
+mod.add(BatchNormalization())
+mod.add(MaxPool2D((2, 2), strides=2, padding='same'))
+mod.add(Conv2D(64, (3, 3), strides=1, padding='same', activation='relu'))
+mod.add(BatchNormalization())
+mod.add(MaxPool2D((2, 2), strides=2, padding='same'))
+mod.add(Conv2D(128, (3, 3), strides=1, padding='same', activation='relu'))
+mod.add(Dropout(0.2))
+mod.add(BatchNormalization())
+mod.add(MaxPool2D((2, 2), strides=2, padding='same'))
+mod.add(Conv2D(256, (3, 3), strides=1, padding='same', activation='relu'))
+mod.add(Dropout(0.2))
+mod.add(BatchNormalization())
+mod.add(MaxPool2D((2, 2), strides=2, padding='same'))
+mod.add(Flatten())
+mod.add(Dense(units=128, activation='relu'))
+mod.add(Dropout(0.2))
+mod.add(Dense(units=1, activation='sigmoid'))
+mod.compile(optimizer="rmsprop", loss='binary_crossentropy', metrics=['accuracy'])
+mod.summary()
+
+lr = ReduceLROnPlateau(monitor='val_accuracy', patience=2, verbose=1, factor=0.3, min_lr=0.000001)
+
+his = mod.fit(dg.flow(x_train, y_train, batch_size=32), epochs=12, validation_data=dg.flow(x_val, y_val), callbacks=[lr])
+
+print("Loss is - ", mod.evaluate(x_test, y_test)[0])
+print("Accuracy is - ", mod.evaluate(x_test, y_test)[1] * 100, "%")
+
+epochs = [i for i in range(12)]
+fi, a = plt.subplots(1, 2)
+train_acc = his.history['accuracy']
+train_loss = his.history['loss']
+val_acc = his.history['val_accuracy']
+val_loss = his.history['val_loss']
+fi.set_size_inches(20, 10)
+
+a[0].plot(epochs, train_acc, 'go-', label='Training Accuracy')
+a[0].plot(epochs, val_acc, 'ro-', label='Validation Accuracy')
+a[0].set_title('Training & Validation Accuracy')
+a[0].legend()
+a[0].set_xlabel("Epochs")
+a[0].set_ylabel("Accuracy")
+
+a[1].plot(epochs, train_loss, 'g-o', label='Training Loss')
+a[1].plot(epochs, val_loss, 'r-o', label='Validation Loss')
+a[1].set_title('Testing Accuracy & Loss')
+a[1].legend()
+a[1].set_xlabel("Epochs")
+a[1].set_ylabel("Training & Validation Loss")
+plt.show()
+
+pre = mod.predict_classes(x_test)
+pre = pre.reshape(1, -1)[0]
+pre[:15]
+
+print(classification_report(y_test, pre, target_names=['Pneumonia (Class 0)', 'Normal (Class 1)']))
+
+cm = confusion_matrix(y_test, pre)
+cm = pd.DataFrame(cm, index=['0', '1'], columns=['0', '1'])
+
+plt.figure(figsize=(10, 10))
+sns.heatmap(cm, cmap="Blues", linecolor='black', linewidth=1, annot=True, fmt='', xticklabels=tags, yticklabels=tags)
+
+correct = np.nonzero(pre == y_test)[0]
+incorrect = np.nonzero(pre != y_test)[0]
+
+i = 0
+for c in correct[:6]:
+    plt.subplot(3, 2, i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(x_test[c].reshape(150, 150), cmap="gray", interpolation='none')
+    plt.title("Predicted {},Actual {}".format(pre[c], y_test[c]))
+    plt.tight_layout()
+    i += 1
+
+i = 0
+for ic in incorrect[:6]:
+    plt.subplot(3, 2, i + 1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(x_test[ic].reshape(150, 150), cmap="gray", interpolation='none')
+    plt.title("Predicted {},Class {}".format(pre[ic], y_test[ic]))
+    plt.tight_layout()
+    i += 1
